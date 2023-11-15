f7532f149e5414884451f52949fda69be6cbff26
WinnerOK <danman339@gmail.com>
2023-11-15 23:00:17 +0300

    select known words

--- wakabi/repository/user.py
+++ wakabi/repository/user.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import asyncpg
 
 
@@ -9,3 +11,20 @@ async def upsert_user_level(conn: asyncpg.Connection, tg_id: int, level: str):
         tg_id,
         level,
     )
+
+
+async def get_known_words(conn: asyncpg.Connection, tg_id: int) -> list[asyncpg.Record]:
+    return await conn.fetch(
+        dedent(
+            """
+            select distinct w.word, w.id
+            from wakabi.words w
+                left join wakabi.word_knowledge wk on w.id = wk.word_id and wk.user_id=$1
+            where
+               wk.word_id is not null or w.language_level <= (select language_level from wakabi.users where tg_id=$1)
+            ;
+            """,
+        ),
+        tg_id,
+    )
+    # you can access data by record['id']
