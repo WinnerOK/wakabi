cb851e8cab4b6b5be4eee008137194cd4cce6e07
WinnerOK <danman339@gmail.com>
2023-11-16 19:34:15 +0300

    reformat

--- wakabi/tg_bot/handlers/help.py
+++ wakabi/tg_bot/handlers/help.py
@@ -15,8 +15,8 @@ def get_help_message() -> str:
         "Available commangs:\n"
         + "\n".join([f"{cmd.command} - {cmd.description}" for cmd in BOT_COMMANDS])
         + "\nYou can also ask me about an unfamiliar word "
-          "or send me the subtitles of your favorite TV series (in .srt format) "
-          "so that I can show you unfamiliar words."
+        "or send me the subtitles of your favorite TV series (in .srt format) "
+        "so that I can show you unfamiliar words."
     )
 
 
